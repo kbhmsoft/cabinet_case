c37d88479f713643c8bc95523e3754c54fcc7b80
Aoyon123 <fahamidulaoyon@gmail.com>
2024-05-26 16:17:52 +0600

    highcourt register details page done

--- resources/views/gov_case/case_register/showDetails.blade.php
+++ resources/views/gov_case/case_register/showDetails.blade.php
@@ -68,10 +68,10 @@
                     {{-- <div class="col-8">fdsafsad</div> --}}
                     {{-- <div class="col-2"><a href="{{ route('messages_group') }}" class="btn btn-primary float-right">Message</a></div> --}}
                     <!--  <div class="col-2">
-                                                                                          @if (Auth::user()->role_id == 2)
+                                                                                                  @if (Auth::user()->role_id == 2)
     <a href="{{ route('messages_group') }}?c={{ $case->id }}" class="btn btn-primary float-right">বার্তা</a>
     @endif
-                                                                                        </div> -->
+                                                                                                </div> -->
                 </div>
             </div>
             <table class="details-pdf-button">
@@ -293,21 +293,24 @@
                         </thead>
                         <tbody>
                             @php $k = 1; @endphp
-                            @foreach ($caseBadi as $badi)
+
+
                                 <tr>
                                     <td>{{ en2bn($k) }}.</td>
                                     <td class="text-center">
-                                        {{-- {{ $badi->name }} --}}
-                                        @if ($badi->name && $case->total_badi_number > 1)
-                                            {{ $badi->name . ' ও অন্যান্য' }}
-                                        @elseif ($badi->name)
-                                            {{ $badi->name }}
+                                        @if($caseBadi->name)
+                                        @if ($caseBadi->name && $case->total_badi_number > 1)
+                                            {{ $caseBadi->name . ' ও অন্যান্য' }}
+                                        @elseif ($caseBadi->name)
+                                            {{ $caseBadi->name }}
                                         @endif
+                                        @endif
+
                                     </td>
-                                    <td class="text-center">{{ $badi->address }}</td>
+                                    <td class="text-center">{{ $caseBadi->address ?? '-' }}</td>
                                 </tr>
                                 @php $k++; @endphp
-                            @endforeach
+
                         </tbody>
                     </table>
 
