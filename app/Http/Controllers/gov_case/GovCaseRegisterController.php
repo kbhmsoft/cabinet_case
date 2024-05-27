31bf33d27a0524adf31fca7bb234a71cd28fa107
Aoyon123 <fahamidulaoyon@gmail.com>
2024-05-27 15:26:32 +0600

    merging problem

--- app/Http/Controllers/gov_case/GovCaseRegisterController.php
+++ app/Http/Controllers/gov_case/GovCaseRegisterController.php
@@ -2736,7 +2736,7 @@ class GovCaseRegisterController extends Controller
 
     public function storeGeneralInfo(Request $request)
     {
-
+    //    dd($request->all());
         $exists = GovCaseRegister::where('case_no', $request->input('case_no'))
             ->where('year', $request->input('case_year'))
             ->where('case_type_id', $request->input('case_category_type'))
--- app/Repositories/gov_case/GovCaseRegisterRepository.php
+++ app/Repositories/gov_case/GovCaseRegisterRepository.php
@@ -59,7 +59,7 @@ class GovCaseRegisterRepository
     public static function storeHighcourtAdalat($caseInfo, $govCaseId)
     {
         foreach ($caseInfo->highcourt_adalat as $key => $val) {
-
+         
             if ($caseInfo->highcourt_adalat[$key] != null) {
                 $highcourtAdalat = new GovCaseHighcourtAdalat();
                 $highcourtAdalat->gov_case_id = $govCaseId;
@@ -596,10 +596,11 @@ class GovCaseRegisterRepository
 
     public static function storeConcernPerson($caseInfo, $govCaseId)
     {
-        // dd($caseInfo);
+
         if ($caseInfo->concernPersonDesignation) {
             foreach ($caseInfo->concernPersonDesignation as $key => $val) {
                 if ($caseInfo->concernPersonDesignation[$key] != null) {
+
                     $concernPrerson = self::checkConcernPersonExist($caseInfo->concern_person_id[$key]);
                     $concernPrerson->gov_case_id = $govCaseId;
                     $concernPrerson->concern_person_designation = $caseInfo->concernPersonDesignation[$key];
